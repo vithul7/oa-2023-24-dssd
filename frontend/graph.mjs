82c48503aadea465fa519bb70b9fbdb6028cf29b
vithul7 <vithulravivarma@gmail.com>
2023-09-18 15:18:47 -0500

    added uncommitted files, working implementation with loading

--- frontend/graph.mjs
+++ frontend/graph.mjs
@@ -1,8 +1,38 @@
 const graphDiv = document.getElementById("graph");
+const generateGraphButton = document.getElementById("generateGraphButton");
+const loadingIndicator = document.getElementById("loadingIndicator");
 
-fetch(
-    "https://oa-2023-24-backend.onrender.com" //use "http://localhost:3000" if running sample express backend locally, or replace with your own backend endpoint url
-).then(async res => {
-    Plotly.newPlot( graphDiv, [ await res.json() ]); 
-})
+const layout = {
+  title: "Forks On Summer2024-Internships Repository",
+  xaxis: {
+    title: "Weeks Since First Fork",
+    showgrid: false,
+    zeroline: false,
+  },
+  yaxis: {
+    title: "Number of Forks",
+    showline: false,
+  },
+};
 
+generateGraphButton.addEventListener("click", () => {
+  // Show loading indicator and disable the button
+  loadingIndicator.style.display = "block";
+  generateGraphButton.disabled = true;
+
+  // Fetch data and generate the graph
+  fetch("http://localhost:3000")
+    .then(async (res) => {
+      const jsonData = await res.json();
+      // Create or update the graph
+      Plotly.newPlot(graphDiv, [jsonData], layout);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    })
+    .finally(() => {
+      // Hide loading indicator and enable the button
+      loadingIndicator.style.display = "none";
+      generateGraphButton.disabled = false;
+    });
+});
